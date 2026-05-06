8015774c6a0b3a2623d40c740a1b6bcf60fdd7e9
federicosimone <federico.simone15@gmail.com>
2026-05-06 16:59:11 +0200

    ok filtro

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -19,16 +19,32 @@ function App() {
 
   }, []);
 
-  console.log(listaPolitici)
+
+
+  const [search, setSearch] = useState("");
+
+
+  const politiciFiltrati = useMemo(() => {
+
+    return listaPolitici.filter(element => {
+      return element.name.toLowerCase().includes(search.toLowerCase()) && element.biography.toLowerCase().includes(search.toLowerCase());
+    })
+  }, [search])
+
+  console.log("filtered", politiciFiltrati)
+
+  const listaDaMostrare = search ? politiciFiltrati : listaPolitici //con il ternario vado a dire che se search non è vuota la variabie "listaDaMostrare" è l'array filtrato
+  //mentre se è vuota, userò l'array listaPolitici, ovvero quello della chiamata API 
 
   return (
     <>
       <h1>Politici Mondiali</h1>
 
-      <input type="text" name="" id="" />
+      <input className="form-control mt-3" type="text" pleaceholder="Cerca..." value={search} onChange={e => setSearch(e.target.value)} name="" id="" />
+      <p className="mt-3">{search ? `Stai cercando: ${search}` : ""}</p>
       <ul className='d-flex flex-wrap g-2 list-unstyled'>
         {
-          listaPolitici.map((politico, i) => {
+          listaDaMostrare.map((politico, i) => {
             return (
               <li key={i}>
                 <div className="card mt-2" style={{ width: "18rem" }}>
